375678c6bdc9f83f1e342cb92cc7bed318353356
Jia Fu Low <jia.fu.low@cern.ch>
2016-11-18 20:32:25 -0600

    Add input, output files, with track fitting

--- copy.sh
+++ copy.sh
@@ -12,3 +12,15 @@ xrdcp roads_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/
 xrdcp roads_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU200.root
 xrdcp roads_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU140.log
 xrdcp roads_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU200.log
+
+xrdcp tracks_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU140.root
+xrdcp tracks_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU200.root
+xrdcp tracks_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU140.log
+xrdcp tracks_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU200.log
+
+xrdcp roads_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_SingleMuonTest_PU0.root
+xrdcp roads_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_SingleMuonTest_PU0.log
+
+xrdcp tracks_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_SingleMuonTest_PU0.root
+xrdcp tracks_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_SingleMuonTest_PU0.log
+
