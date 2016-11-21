62a4855c8f32a27fd0b8c5857ab47ab4cd87d0d8
Jia Fu Low <jia.fu.low@cern.ch>
2016-11-21 09:12:56 -0600

    Update

--- copy.sh
+++ copy.sh
@@ -1,26 +1,26 @@
-xrdcp stubs_oc_tt25_200M_1.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/stubs_oc_tt25_200M_1.root
-xrdcp stubs_oc_tt25_200M_2.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/stubs_oc_tt25_200M_2.root
-xrdcp stubs_oc_tt25_200M_1.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/stubs_oc_tt25_200M_1.log
-xrdcp stubs_oc_tt25_200M_2.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/stubs_oc_tt25_200M_2.log
+xrdcp -f stubs_oc_tt25_200M_1.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f stubs_oc_tt25_200M_2.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f stubs_oc_tt25_200M_1.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f stubs_oc_tt25_200M_2.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
-xrdcp patternBank_oc_tt25_sf1_nz8_pt3_200M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/patternBank_oc_tt25_sf1_nz8_pt3_200M.root
-xrdcp patternBank_oc_tt25_sf1_nz8_pt3_400M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/patternBank_oc_tt25_sf1_nz8_pt3_400M.root
-xrdcp patternBank_oc_tt25_sf1_nz8_pt3_200M.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/patternBank_oc_tt25_sf1_nz8_pt3_200M.log
-xrdcp patternBank_oc_tt25_sf1_nz8_pt3_400M.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/patternBank_oc_tt25_sf1_nz8_pt3_400M.log
+xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_200M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_400M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_200M.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_400M.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
-xrdcp roads_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU140.root
-xrdcp roads_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU200.root
-xrdcp roads_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU140.log
-xrdcp roads_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_TTbar_PU200.log
+xrdcp -f roads_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
-xrdcp tracks_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU140.root
-xrdcp tracks_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU200.root
-xrdcp tracks_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU140.log
-xrdcp tracks_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_TTbar_PU200.log
+xrdcp -f tracks_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
-xrdcp roads_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_SingleMuonTest_PU0.root
-xrdcp roads_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/roads_SingleMuonTest_PU0.log
+xrdcp -f roads_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
-xrdcp tracks_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_SingleMuonTest_PU0.root
-xrdcp tracks_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/tracks_SingleMuonTest_PU0.log
+xrdcp -f tracks_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
--- doit.sh
+++ doit.sh
@@ -1,5 +1,5 @@
 jobid=1
-amsim -C -i input_SingleMuon_oc_tt25_${jobid}.txt -o stubs_oc_tt25_200M_${jobid}.root -v 2 -n 200000000 >& stubs_oc_tt25_200M_${jobid}.log
+amsim -C -i input/input_SingleMuon_oc_tt25_${jobid}.txt -o stubs_oc_tt25_200M_${jobid}.root -v 2 -n 200000000 >& stubs_oc_tt25_200M_${jobid}.log
 
 amsim -B -i input/stubs_oc_tt25_400M.txt -o patternBank_oc_tt25_sf1_nz8_pt3_400M.root -v 2 -s sf1_nz8 --minPt 3 >& patternBank_oc_tt25_sf1_nz8_pt3_400M.log
 
@@ -7,6 +7,10 @@ amsim -R -i input/input_SingleMuonTest_PU0.txt -b bank/patternBank_oc_tt25_sf1_n
 amsim -R -i input/input_TTbar_PU140.txt -b bank/patternBank_oc_tt25_sf1_nz8_pt3_400M.txt -o roads_TTbar_PU140.root -v 2 -s sf1_nz8 -n 30000 >& roads_TTbar_PU140.log
 amsim -R -i input/input_TTbar_PU200.txt -b bank/patternBank_oc_tt25_sf1_nz8_pt3_400M.txt -o roads_TTbar_PU200.root -v 2 -s sf1_nz8 -n 30000 >& roads_TTbar_PU200.log
 
-amsim -T -i output/roads_SingleMuonTest_PU0.txt -o tracks_SingleMuonTest_PU0.root -v 2 >& tracks_SingleMuonTest_PU0.log &
-amsim -T -i output/roads_TTbar_PU140.txt -o tracks_TTbar_PU140.root -v 2 >& tracks_TTbar_PU140.log &
-amsim -T -i output/roads_TTbar_PU200.txt -o tracks_TTbar_PU200.root -v 2 >& tracks_TTbar_PU200.log &
+amsim -T -i output/roads_SingleMuonTest_PU0.txt -o tracks_SingleMuonTest_PU0.root -v 2 --maxPatterns 5532635 >& tracks_SingleMuonTest_PU0.log
+amsim -T -i output/roads_TTbar_PU140.txt -o tracks_TTbar_PU140.root -v 2 --maxPatterns 5532635 >& tracks_TTbar_PU140.log
+amsim -T -i output/roads_TTbar_PU200.txt -o tracks_TTbar_PU200.root -v 2 --maxPatterns 5532635 >& tracks_TTbar_PU200.log
+
+amsim -P -i bank/patternBank_oc_tt25_sf1_nz8_pt3_400M.txt -o m8_patternBank_oc_tt25_sf1_nz8_pt3_400M.root -v 2 >& m8_patternBank_oc_tt25_sf1_nz8_pt3_400M.log 
+
+amsim -Q -i output/roads_TTbar_PU140.txt -b bank/m8_patternBank_oc_tt25_sf1_nz8_pt3_400M.txt -o m8_roads_TTbar_PU140.root -v 2 >& m8_roads_TTbar_PU140.log 
