6313e72582a685bbab9f297f664d9e4cbb116c85
Jia Fu Low <jia.fu.low@cern.ch>
2016-11-21 22:55:48 -0600

    Update

--- copy.sh
+++ copy.sh
@@ -1,26 +1,41 @@
 xrdcp -f stubs_oc_tt25_200M_1.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f stubs_oc_tt25_200M_1.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 xrdcp -f stubs_oc_tt25_200M_2.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f stubs_oc_tt25_200M_1.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f stubs_oc_tt25_200M_2.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f stubs_oc_tt25_200M_2.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
 xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_200M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_200M.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_400M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_200M.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_400M.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-
-xrdcp -f roads_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f roads_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f roads_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f roads_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-
-xrdcp -f tracks_TTbar_PU140.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f tracks_TTbar_PU200.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f tracks_TTbar_PU140.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f tracks_TTbar_PU200.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f patternBank_oc_tt25_sf1_nz8_pt3_400M.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
 xrdcp -f roads_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f roads_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_SingleMuonTest_PU0.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU140.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU140.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU200.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f roads_TTbar_PU200.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
 xrdcp -f tracks_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
-xrdcp -f tracks_SingleMuonTest_PU0.log root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_SingleMuonTest_PU0.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU140.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU140.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU200.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f tracks_TTbar_PU200.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+
+xrdcp -f m8_patternBank_oc_tt25_sf1_nz8_pt3_400M.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_patternBank_oc_tt25_sf1_nz8_pt3_400M.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+
+xrdcp -f m8_roads_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_roads_SingleMuonTest_PU0.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_roads_TTbar_PU140.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_roads_TTbar_PU140.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_roads_TTbar_PU200.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_roads_TTbar_PU200.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+
+xrdcp -f m8_tracks_SingleMuonTest_PU0.root root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_tracks_SingleMuonTest_PU0.log  root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_tracks_TTbar_PU140.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_tracks_TTbar_PU140.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_tracks_TTbar_PU200.root        root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
+xrdcp -f m8_tracks_TTbar_PU200.log         root://cmseos:1094//store/user/l1upgrades/SLHC/GEN/620_SLHC25p3_results/jftest1/
 
